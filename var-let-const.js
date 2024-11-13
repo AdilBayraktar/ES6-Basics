ab820b41fd5083cec24cc6e2e3e2050739034873
adilBayraktar <adilbayraktar1997@gmail.com>
2024-11-14 00:12:08 +0300

    var-let-const

---
+++ var-let-const.js
@@ -0,0 +1,17 @@
+// var (Global variables, able to declare after initialization, able to reassign)
+var var_name = "Var Name";
+console.log(var_name);
+var_name = "Variable Name";
+console.log(var_name);
+
+//let (Block variables, able to reassign)
+let let_name = "Let Name";
+console.log(let_name);
+let_name = "Let Name new";
+console.log(let_name);
+
+//const (Block variables, constant variable)
+const const_name = "Constant Name";
+console.log(const_name);
+const_name = "Constant Name new";
+console.log(const_name); // will return error
