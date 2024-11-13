2081165abb28a1ac7b6948bc04df77068c1d1f05
adilBayraktar <adilbayraktar1997@gmail.com>
2024-11-14 00:38:40 +0300

    Arrow Function

---
+++ arrowFunction.js
@@ -0,0 +1,12 @@
+// Normal Function
+
+function hello(name) {
+  console.log(`Hello Normal, ${name}`);
+}
+hello("Adil BAYRAKTAR"); // Output: Hello Normal, Adil BAYRAKTAR
+
+// Arrow Function
+
+const helloArrow = (name) => console.log(`Hello From Arrow, ${name}`);
+
+helloArrow("Adil BAYRAKTAR"); // Output: Hello From Arrow, Adil BAYRAKTAR
